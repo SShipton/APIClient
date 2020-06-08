5de8f081521c42a544bc85a3cd8dadb3292b4f5a
Gavin Stark <gavin@gstark.com>
2020-06-08 13:29:23 -0400

    Just a few small changes are necessary
    Here are the few small changes needed to make your API client work.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
@@ -8,71 +8,72 @@ using ConsoleTables;
 
 namespace APIClient
 {
-    class Program
+    class Brewery
     {
-        class Brewery
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+
+        [JsonPropertyName("city")]
+        public string city { get; set; }
+
+
+        [JsonPropertyName("state")]
+        public string state { get; set; }
+
+
+        [JsonPropertyName("country")]
+        public string country { get; set; }
+
+
+        [JsonPropertyName("brewery type")]
+        public string brewery_type { get; set; }
+
+
+        [JsonPropertyName("street")]
+        public string street { get; set; }
+
+        [JsonPropertyName("postal code")]
+        public int postal_code { get; set; }
+
+        [JsonPropertyName("longitude")]
+        public string longitude { get; set; }
+
+        [JsonPropertyName("latitude")]
+        public string latitude { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string phone { get; set; }
+
+        [JsonPropertyName("website url")]
+        public string website_url { get; set; }
+
+        [JsonPropertyName("updated at")]
+        public DateTime updated_at { get; set; }
+
+        [JsonPropertyName("tag list")]
+        public string tag_list { get; set; }
+
+        [JsonPropertyName("complete")]
+        public bool Complete { get; set; }
+
+        public string CompletedStatus
         {
-            [JsonPropertyName("id")]
-            public int id { get; set; }
-
-
-            [JsonPropertyName("name")]
-            public string name { get; set; }
-
-
-            [JsonPropertyName("city")]
-            public string city { get; set; }
-
-
-            [JsonPropertyName("state")]
-            public string state { get; set; }
-
-
-            [JsonPropertyName("country")]
-            public string country { get; set; }
-
-
-            [JsonPropertyName("brewery type")]
-            public string brewery_type { get; set; }
-
-
-            [JsonPropertyName("street")]
-            public string street { get; set; }
-
-            [JsonPropertyName("postal code")]
-            public int postal_code { get; set; }
-
-            [JsonPropertyName("longitude")]
-            public int longitude { get; set; }
-
-            [JsonPropertyName("latitude")]
-            public int latitude { get; set; }
-
-            [JsonPropertyName("phone")]
-            public int phone { get; set; }
-
-            [JsonPropertyName("website url")]
-            public string website_url { get; set; }
-
-            [JsonPropertyName("updated at")]
-            public DateTime updated_at { get; set; }
-
-            [JsonPropertyName("tag list")]
-            public string tag_list { get; set; }
-
-            [JsonPropertyName("complete")]
-            public bool Complete { get; set; }
-
-            public string CompletedStatus
+            get
             {
-                get
-                {
-                    return Complete ? "completed" : "not completed";
-                }
+                return Complete ? "completed" : "not completed";
             }
         }
+    }
 
-        static async Task<string> Main(string[] args)
+    class Program
+    {
+
+        static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
